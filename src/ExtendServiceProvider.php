fdbfc5cb7e1009aa7bcf1085ceba51c91f12a641
sang <tieuha.801@gmail.com>
2024-02-28 23:44:35 +0700

    fix module

--- src/ExtendServiceProvider.php
+++ src/ExtendServiceProvider.php
@@ -9,60 +9,84 @@ use RecursiveDirectoryIterator;
 use RecursiveIteratorIterator;
 use Illuminate\Support\Facades\Route;
 
+
 class ExtendServiceProvider extends IlluminateServiceProvider
 {
-    public function boot() {}
+    public function boot()
+    {
+    }
 
     public function register(): void
     {
-        $listModule = array_map('basename', File::directories(__DIR__ . '/plugins'));
-        foreach ($listModule as $module) {
-            $publishName = Str::of($module)->before("Module")->lower();
-            // load config
-            $listConfig = $this->getRecursiveFileInDirectory(__DIR__ . '/plugins/' . $module . '/config');
-            foreach ($listConfig as $config) {
-                $this->mergeConfigFrom($config, "{$publishName}_config");
-            }
-            // load router
-            $listRoute = $this->getRecursiveFileInDirectory(__DIR__ . '/plugins/' . $module . '/routes');
-            foreach ($listRoute as $route) {
-                if (Str::match('/.*web.php$/', $route)) {
-                    $this->loadRoutesFrom($route);
+        if ($this->checkIsDirectory(__DIR__ . "/../../../../platform/modules")) {
+            $modulePath = __DIR__ . "/../../../../platform/modules";
+            $listModule = array_map('basename', File::directories($modulePath));
+            foreach ($listModule as $module) {
+                $publishName = Str::of($module)->before("Module")->lower();
+                // load config
+                if ($this->checkIsDirectory($modulePath . "/" . $module . "/config")) {
+                    $listConfig = $this->getRecursiveFileInDirectory($modulePath . "/" . $module . "/config");
+                    foreach ($listConfig as $config) {
+                        $this->mergeConfigFrom($config, "{$publishName}_config");
+                    }
                 }
-                if (Str::match('/.*api.php$/', $route)) {
-                    Route::prefix('api')->group(fn() => $this->loadRoutesFrom($route));
+                // load router
+                if ($this->checkIsDirectory($modulePath . "/" . $module . "/routes")) {
+                    $listRoute = $this->getRecursiveFileInDirectory($modulePath . "/" . $module . "/routes");
+                    foreach ($listRoute as $route) {
+                        if (Str::match('/.*web.php$/', $route)) {
+                            $this->loadRoutesFrom($route);
+                        }
+                        if (Str::match('/.*api.php$/', $route)) {
+                            Route::prefix("api")->group(fn () => $this->loadRoutesFrom($route));
+                        }
+                    }
+                }
+                // load database migration
+                if ($this->checkIsDirectory($modulePath . "/" . $module . "/database/migrations")) {
+                    $listMigration = $this->getRecursiveFileInDirectory($modulePath . "/" . $module . "/database/migrations");
+                    foreach ($listMigration as $migration) {
+                        $this->loadMigrationsFrom($migration);
+                    }
+                }
+                // load database seeders
+                if ($this->checkIsDirectory($modulePath . "/" . $module . "/database/seeders")) {
+                    $listSeeder = $this->getRecursiveFileInDirectory($modulePath . "/" . $module . "/database/seeders");
+                    foreach ($listSeeder as $seeder) {
+                        $this->loadMigrationsFrom($seeder);
+                    }
+                }
+                // load view
+                if ($this->checkIsDirectory($modulePath . "/" . $module . "/resources/views")) {
+                    $this->loadViewsFrom($modulePath . "/" . $module . "/resources/views", "{$publishName}_view");
+                }
+                // $this->publishes([__DIR__. '/plugins/' . $module . '/resources/views' .'/public' => public_path('vendor/courier')], 'public');
+                // load providers
+                if ($this->checkIsDirectory($modulePath . "/" . $module . "/app/Providers")) {
+                    $listProvider = $this->getRecursiveFileInDirectory($modulePath . "/" . $module . "/app/Providers");
+                    foreach ($listProvider as $provider) {
+                        $provider = preg_replace("/\//", "\\", $provider);
+                        // dd($provider);
+                        // $provider = preg_replace("/\..*/", "", Str::afterLast($provider, "/"));
+                        // dd($modulePath . "/" . $module . $provider);
+                        // $this->app->register($provider);
+                    }
+                }
+                // load function helper
+                if ($this->checkIsDirectory($modulePath . "/" . $module . "/app/Helpers/FunctionHelper")) {
+                    $listHelper = $this->getRecursiveFileInDirectory($modulePath . "/" . $module . "/app/Helpers/FunctionHelper");
+                    foreach ($listHelper as $helper) {
+                        require_once($helper);
+                    }
+                }
+                // load lang
+                if ($this->checkIsDirectory($modulePath . "/" . $module . "/resources/lang/")) {
+                    $this->loadTranslationsFrom($modulePath . "/" . $module . "/resources/lang/", "{$publishName}_lang");
+                    // $this->publishes([
+                    //     $modulePath . "/" . $module . "/resources/lang/"  => resource_path("lang/vendor/{$publishName}"),
+                    // ]);
                 }
             }
-            // load database migration
-            $listMigration = $this->getRecursiveFileInDirectory(__DIR__ . '/plugins/' . $module . '/database/migrations');
-            foreach ($listMigration as $migration) {
-                $this->loadMigrationsFrom($migration);
-            }
-
-            // load database seeders
-            $listSeeder = $this->getRecursiveFileInDirectory(__DIR__ . '/plugins/' . $module . '/database/seeders');
-            foreach ($listSeeder as $seeder) {
-                $this->loadMigrationsFrom($seeder);
-            }
-            // load view
-            $this->loadViewsFrom(__DIR__ . '/plugins/' . $module . '/resources/views', "{$publishName}_view");
-//            $this->publishes([__DIR__. '/plugins/' . $module . '/resources/views' .'/public' => public_path('vendor/courier')], 'public');
-            // load providers
-            $listProvider = $this->getRecursiveFileInDirectory(__DIR__ . '/plugins/' . $module . '/app/Providers');
-            foreach ($listProvider as $provider) {
-                $provider = preg_replace("/\..*/", "", Str::afterLast($provider, '/'));
-                $this->app->register("Plugin\plugins\\{$module}\\app\Providers\\{$provider}");
-            }
-            // load function helper
-            $listHelper = $this->getRecursiveFileInDirectory(__DIR__ . '/plugins/' . $module . '/app/Helpers/FunctionHelper');
-            foreach ($listHelper as $helper) {
-                require_once($helper);
-            }
-            // load lang
-            $this->loadTranslationsFrom(__DIR__ . '/plugins/' . $module . '/resources/lang/', "{$publishName}_lang");
-            $this->publishes([
-                __DIR__ . '/modules/' . $module . '/resources/lang/'  => resource_path("lang/vendor/{$publishName}"),
-            ]);
         }
     }
 
@@ -81,5 +105,10 @@ class ExtendServiceProvider extends IlluminateServiceProvider
         }
 
         return $files;
+    }
+
+    private function checkIsDirectory(string $path)
+    {
+        return is_dir($path);
     }
 }
